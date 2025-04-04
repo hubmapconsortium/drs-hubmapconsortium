3d16ad9dcdfe337107d180ae2e1b89718c56b81f
Juan Puerto <=>
2025-04-04 11:34:20 -0400

    fix: don't make access_id None

--- app.py
+++ app.py
@@ -100,7 +100,7 @@ def get_drs_object(drs_uuid):
                 {
                     "type": "https",
                     "access_url": {"url": f"https://{ACCESS_DOMAIN}{access_path}"},
-                    "access_id": None
+                    "access_id": "https"
                 }
             ]
             body["checksums"] = [{"checksum": object[0]["md5"], "type": "md5"}]
